f8cd00958d126784edaa7c8827a2eb810b9d3b1a
Katsu Rian <redkrsc@gmail.com>
2024-04-18 22:14:32 -0400

    Beginning of auth stuff

--- init1.py
+++ init1.py
@@ -46,7 +46,34 @@ def lookUpFlight():
 
 
 #Authenticates the login
-@app.route('/loginAuth', methods=['GET', 'POST'])
+@app.route('/customerLoginAuth', methods=['GET', 'POST'])
+def loginAuth():
+	#grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM user WHERE username = %s and password = %s'
+	cursor.execute(query, (username, password))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	cursor.close()
+	error = None
+	if(data):
+		#creates a session for the the user
+		#session is a built in
+		session['username'] = username
+		return redirect(url_for('home'))
+	else:
+		#returns an error message to the html page
+		error = 'Invalid login or username'
+		return render_template('login.html', error=error)
+	
+#Authenticates the login
+@app.route('/customerLoginAuth', methods=['GET', 'POST'])
 def loginAuth():
 	#grabs information from the forms
 	username = request.form['username']
